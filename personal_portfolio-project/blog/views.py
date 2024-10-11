b6307160dd5d921d1e16f671ba1367ffb2108fdf
JasierDeen <jasierdeen70@gmail.com>
2024-10-11 18:42:12 +0530

    Bootstrap, style changes

--- personal_portfolio-project/blog/views.py
+++ personal_portfolio-project/blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
-def all_blog(request):
-    blogs = Blog.objects.order_by("-created_at")[:5]
+def all_blogs(request):
+    blogs = Blog.objects.order_by("-created_at")
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
 def detail(request, blog_id):
